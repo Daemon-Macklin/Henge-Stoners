a7b7e9c287c756339aeec74909e6512e616776fa
Daemon-Macklin <daemon.macklin@outlook.com>
2019-10-26 18:16:40 +0100

    Finishing comments

--- app/src/main/java/com/example/hengestoners/models/UserJSONStore.kt
+++ app/src/main/java/com/example/hengestoners/models/UserJSONStore.kt
@@ -10,11 +10,15 @@ import org.jetbrains.anko.info
 import java.lang.Exception
 import java.net.PasswordAuthentication
 import java.util.*
+// Functions to manage users and their hillforts
+
 
 val JSON_FILE = "hillFortData.json"
 val gsonBuilder = GsonBuilder().setPrettyPrinting().create()
 val listType = object : TypeToken<java.util.ArrayList<UserModel>>() {}.type
 
+
+// Function to randomly generate id for users or hillforts
 fun generateRandomId(): Long {
     return Random().nextLong()
 }
@@ -23,7 +27,8 @@ class UserJSONStore: UserStore, AnkoLogger {
 
     val context: Context
     var users = mutableListOf<UserModel>()
-    
+
+    // When created see if json file exists and load it
     constructor(context: Context) {
         this.context = context
         if(exists(context, JSON_FILE)) {
@@ -31,12 +36,16 @@ class UserJSONStore: UserStore, AnkoLogger {
         }
     }
 
+    // Method to find all users
     override fun findAll(): List<UserModel> {
         return users
     }
 
+    // Method to create a new user
     override fun create(user: UserModel): Boolean {
+        // Check if the email is already used
         if(findByEmail(user.email) == null) {
+            // If not add the user, save the data and return true
             user.id = generateRandomId()
             user.password = encryptPassword(user.password, user.salt)
             info(user)
@@ -44,76 +53,122 @@ class UserJSONStore: UserStore, AnkoLogger {
             serialize()
             return true
         }
+        // else return false
         return false
     }
 
+    // Method to update user email or password
     override fun updateDetails(user: UserModel, email: String, userName: String): Boolean {
+        // Check if the email is already in use
         if(findByEmail(email) == null) {
+
+            // If it is not then find
             val foundUser: UserModel? = users.find { p -> p.id == user.id }
             if (foundUser != null) {
+                // If the user is found update, save and return true
                 foundUser.userName = userName
                 foundUser.email = email
                 serialize()
                 return true
             }
         }
+
+        // Else return false
         return false
     }
 
+    // Method to update the user password
     override fun updatePassword(user: UserModel, curPass: String, newPass: String): Boolean {
+
+        // Find the user
         val foundUser: UserModel? = users.find { p -> p.id == user.id }
         if(foundUser != null) {
+
+            // If found verify the password
             val result = userAuth(user, curPass)
+
             return if (result) {
+
+                // If the password is verified then set the encrypted password as the new user password
+                // save and return true
                 user.password = encryptPassword(newPass, user.salt)
                 serialize()
                 true
             } else {
+                // else return false
                 false
             }
         }
+        // Return false
         return false
     }
 
+    // Function to remove user
     override fun remove(user: UserModel, pass: String): Boolean {
+        // Find the user
         val foundUser: UserModel? = users.find { foundUser -> foundUser.id == user.id }
         if(foundUser != null && userAuth(user, pass)) {
+
+            // If the user is found and the password is verified remove the user, save and return true
             users.remove(foundUser)
             serialize()
             return true
         }
+
+        // Else return false
         return false
     }
 
+
+    // Funtion to login user
     override fun login(email: String, pass: String): Boolean {
-        val user: UserModel? = users.find { user -> user.email == email }
+        // Find the user
+        val user = findByEmail(email)
+
         if(user != null && userAuth(user, pass)) {
+
+            // If the user is found and the password verified then return true
             return true
         }
+
+        // Else return false
         return false
     }
 
+    // Function to find user by email address
     override fun findByEmail(email: String): UserModel? {
         return users.find { user -> user.email == email }
     }
 
+    // Function to log all users
     override fun logAll() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        users.forEach {
+            info(it)
+        }
     }
 
+    // Function to find all a given users fillforts
     override fun findAllHillForts(user: UserModel): List<HillFortModel> {
         return users.find { userModel: UserModel ->  userModel == user }!!.hillForts
     }
 
+    // Function to create a new hillfort
     override fun createHillFort(user: UserModel, hillFort: HillFortModel) {
+
+        // Give hillfort id, add to users list then save
         hillFort.id = generateRandomId()
         user.hillForts.add(hillFort)
         serialize()
     }
 
+    // Function to update a hillfort
     override fun updateHillFort(user: UserModel, hillFort: HillFortModel){
+
+        // Find the hillfort
         var foundHillFort: HillFortModel? = user.hillForts.find { p -> p.id == hillFort.id }
+
         if (foundHillFort != null){
+            // If the hillfort it found. update and save
             foundHillFort.title = hillFort.title
             foundHillFort.description = hillFort.description
             foundHillFort.location["lat"] = hillFort.location["lat"].toString().toDouble()
@@ -127,22 +182,24 @@ class UserJSONStore: UserStore, AnkoLogger {
         }
     }
 
+    // Function to log all hillforts
     override fun logAllHillForts(user: UserModel){
         user.hillForts.forEach { info("$it") }
     }
 
+    // Function to remove hillfort
     override fun removeHillFort(user: UserModel, hillFort: HillFortModel) {
-        val index = user.hillForts.indexOf(hillFort)
-        user.hillForts.removeAt(index)
+        user.hillForts.remove(hillFort)
         serialize()
     }
 
-
+    // Function to write data to the json file
     private fun serialize() {
         val jsonString = gsonBuilder.toJson(users, listType)
         write(context, JSON_FILE, jsonString)
     }
 
+    // Function to read data from json file
     private fun deserialize() {
         val jsonString = read(context, JSON_FILE)
         users = Gson().fromJson(jsonString, listType)
--- app/src/main/java/com/example/hengestoners/models/UserModel.kt
+++ app/src/main/java/com/example/hengestoners/models/UserModel.kt
@@ -5,7 +5,7 @@ import com.example.hengestoners.helpers.generateSalt
 import kotlinx.android.parcel.Parcelize
 import kotlinx.android.parcel.RawValue
 import kotlin.collections.ArrayList
-
+// Model to store user data
 @Parcelize
 data class UserModel(var id: Long = 0,
                          var userName: String = "",
