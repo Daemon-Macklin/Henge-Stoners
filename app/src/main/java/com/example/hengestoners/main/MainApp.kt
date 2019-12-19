f59e2e47b632bcf013fb69361dfc0d47aaade424
Daemon-Macklin <daemon.macklin@outlook.com>
2019-12-19 20:06:45 +0000

    Moving jsonstore

--- app/src/main/java/com/example/hengestoners/main/MainApp.kt
+++ app/src/main/java/com/example/hengestoners/main/MainApp.kt
@@ -2,9 +2,9 @@ package com.example.hengestoners.main
 
 import android.app.Application
 import com.example.hengestoners.models.*
+import com.example.hengestoners.models.JSON.UserJSONStore
 import org.jetbrains.anko.AnkoLogger
 import org.jetbrains.anko.info
-import java.time.LocalDate
 
 class MainApp : Application(), AnkoLogger {
 
